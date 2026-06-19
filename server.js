02e5342f7499002ce55c8c78e597cea7f95fa3f3
Arisha-Khan23 <arishankhanswb@gmail.com>
2026-06-19 14:50:16 +0500

    fix port

--- server.js
+++ server.js
@@ -12,11 +12,10 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.log("MongoDB error:", err));
 
 app.use(express.json());
-
-const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.json({ message: "Online Bookstore API is running!" });
 });
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
