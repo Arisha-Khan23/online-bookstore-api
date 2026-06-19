076cc8ea70851d44f2a856444a2d31c23a836222
Arisha-Khan23 <arishankhanswb@gmail.com>
2026-06-19 14:21:24 +0500

    initial commit

--- server.js
+++ server.js
@@ -1,46 +1,18 @@
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/.env' });
 
 const express = require("express");
 const mongoose = require("mongoose");
 
-const logger = require("./middleware/logger");
-const bookRoutes = require("./routes/books");
-
-console.log("logger =", logger);
-console.log("bookRoutes =", bookRoutes);
-console.log("MONGO_URI:", process.env.MONGO_URI);
 const app = express();
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log("MongoDB error:", err));
 
 app.use(express.json());
 
-app.use(logger);
-
-app.use("/api/books", bookRoutes);
-
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("MongoDB Connected");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-app.use((req, res) => {
-    res.status(404).json({
-        message: "Route not found"
-    });
-});
-
-app.use((err, req, res, next) => {
-    console.error(err);
-
-    res.status(500).json({
-        message: err.message || "Internal Server Error"
-    });
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
 });
